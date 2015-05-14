f6ae5c45abad8658c288f7797d6ff4da82f898b9
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-13 21:37:58 -0500

    refactored entirely

--- calculator.js
+++ calculator.js
@@ -1,49 +1,99 @@
 
-
 function Calculator() {
-	this.pi = Math.PI;
+
 	this.display = [0];
 	this.m = 0;
 	this.temp = "0";
 	
-	this.multiply = function() {
-		var a = this.display[0] * this.display[2];
-		this.display = [a];
+	this.compute = function() {
+		this.temp = "0";
+		switch(this.display[1]) {
+			case "+":
+				return this.display = [this.display[0] + this.display[2]];
+			case "-":
+				return this.display = [this.display[0] - this.display[2]];
+			case "/":
+				return this.display = [this.display[0] / this.display[2]];
+			case "*":
+				return this.display = [this.display[0] * this.display[2]];
+			case "^":
+				return this.display = [Math.pow(this.display[0], this.display[2])];
+		}
+	};
+	
+	this.equals = function() {
+		console.log(this.display);
+		if (this.display.length === 2) {
+			this.display[2] = this.display[0];
+			this.compute();
+		}
+		if (this.display.length === 3) {
+			this.compute();
+		}
+	};
+	
+	this.num = function(id) {
+		this.temp += id;
+		if (isNaN(this.display[this.display.length - 1])) {
+			this.display[this.display.length] = parseFloat(this.temp);
+		}
+		else {
+			this.display[this.display.length - 1] = parseFloat(this.temp);
+		}
+	};
+	
+	this.dot = function() {
+		if (this.display[this.display.length - 1] % 1 === 0) {
+			this.temp += ".";
+			this.display[this.display.length - 1] = parseFloat(this.temp);
+		}
+	};
+	
+	this.pi = function() {
+		if (isNaN(this.display[this.display.length - 1])) {
+			this.display[this.display.length] = Math.PI;
+		}
+		else {
+			this.display[this.display.length - 1] = Math.PI;
+		}
+	};
+	
+	this.back = function() {
+		this.temp = this.temp.substring(0, this.temp.length - 1);
+		this.display[this.display.length - 1] = parseFloat(this.temp);
+	};
+	
+	this.populateOperators = function(code) {
+		this.temp = "0";
+		this.display[this.display.length] = code;
+	};
+	
+	
+	this.clear = function() {
+		this.temp = "0";
+		this.display = [0];
 	};
 
-	this.add = function() {
-		var a = this.display[0] + this.display[2];
-		this.display = [a];   
-	};
-
-	this.divide = function() {
-		var a = this.display[0] / this.display[2];
-		this.display = [a]; 
-	};
-
-	this.subtract = function(a, b) {
-		var a = this.display[0] - this.display[2];
-		this.display = [a];     
-	};
-
-	this.squareRoot = function() {
-		var a = Math.sqrt(this.display[0]);
-		this.display = [a];
-	};
-
-	this.power = function() {
-		var a = Math.pow(this.display[0], this.display[2]);
-		this.display = [a];
+	this.sqroot = function() {
+		this.display = Math.sqrt(this.display[this.display.length - 1]);
 	};
 
 	this.negate = function() {
 		this.display[this.display.length -1] *= (-1);
+		this.temp = this.display[this.display.length -1].toString();
 	};
 
 	this.inverse = function() {
-		var a = 1 / (this.display[this.display.length -1]);
-		console.log(a);
-		this.display[this.display.length -1] = a;
+		console.log(this.display);
+		if (this.display[this.display.length -1] === 0) {
+			console.log(this.display + "first");
+			alert("Cannot divide by 0.");
+		}
+		else {
+			console.log(this.display + "second");
+			this.display[this.display.length -1] = (1 / (this.display[this.display.length -1]));
+			console.log(this.display + "secondafter");
+		}
 	};
 }
 
@@ -51,40 +101,24 @@ var calc = new Calculator();
 
 
 function input(element) {
-	if (/\d/.test(element.id)) {
-		calc.temp += element.id;
-		calc.display[calc.display.length - 1] = parseFloat(calc.temp);
+	if (/^[0-9]$/.test(element.innerHTML)) {
+		calc.num(element.innerHTML);
 	}
-	else if (element.id === "clear") {
-		calc.display = [0];
-	}
-	else if (element.id === "negate") {
-		calc.negate();
-		console.log(calc.temp);
-		console.log(calc.display);
-		calc.display[calc.display.length - 1] = parseFloat(calc.temp);
-	}
-	else if (element.id === "inverse") {
-		calc.inverse();
-		console.log(calc.temp);
-		console.log(calc.display);
-		calc.display[calc.display.length - 1] = parseFloat(calc.temp);
-	}
-	else if (element.id === "dot") {
-		calc.temp += ".";
-		calc.display[calc.display.length - 1] = parseFloat(calc.temp);
-	}
-	else if (element.id === "m") {
-		calc.m = calc.display[calc.display.length - 1];
-	}
-	else if (element.id === "back") {
-		if (calc.display[calc.display.length - 1].length > 0) {
-			console.log(parseFloat(calc.display[calc.display.length - 1].slice(0, -1)));
-		}
+	else if (containsID(["add", "subtract", "multiply", "divide", "power"], element.id)) {
+		calc.populateOperators(element.innerHTML);
 	}
 	else {
-		calc.display[calc.display.length] = calc.pi;
+		calc[element.id]();
 	}
 	document.getElementById("display").innerHTML = calc.display.join(" ");
 }
 
+
+function containsID(array, id) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === id) {
+            return true;
+        }
+    }
+    return false;
+}
