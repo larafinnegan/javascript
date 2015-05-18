9ae0bc4c45efba4b44a9cdcd42ceaf3424a5bea6
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-17 20:38:27 -0500

    refactored this thing completely...not sure if it's for the better...

--- calculator.js
+++ calculator.js
@@ -1,158 +1,185 @@
+var calc = {
 
-function Calculator() {
-
-	this.display = [0];
-	this.m = 0;
-	this.temp = "0";
-	this.result = 0;
+	m: 0,
 	
-	this.compute = function() {
-		this.temp = "0";
-		switch(this.display[1]) {
+	compute: function(array) {
+		array[0] = parseFloat(array[0]);
+		array[2] = parseFloat(array[2]);
+
+		switch(array[1]) {
 			case "+":
-				return this.display = this.result = [this.display[0] + this.display[2]];
+				return array[0] + array[2];
 			case "-":
-				return this.display = this.result = [this.display[0] - this.display[2]];
+				return array[0] - array[2];
 			case "/":
-				return this.display = this.result = [this.display[0] / this.display[2]];
+				return array[0] / array[2];
 			case "*":
-				return this.display = this.result = [this.display[0] * this.display[2]];
+				return array[0] * array[2];
 			case "^":
-				return this.display = this.result = [Math.pow(this.display[0], this.display[2])];
+				return Math.pow(array[0], array[2]);
 		}
-	};
+	},
 	
-	this.memset = function() {
-		if (isNaN(this.display[this.display.length - 1])) {
-			this.m = this.display[this.display.length - 2];
+	memset: function() {
+		this.m = box.current;
+	},
+	
+	memrecall: function() {
+		box.current = this.m;
+	},
+		
+	pi: function() {
+		if (isNaN(box.inputs[box.inputs.length - 1])) {
+			box.inputs[box.inputs.length] = box.current = Math.PI;
 		}
 		else {
-			this.m = this.display[this.display.length - 1];
+			box.inputs[box.inputs.length - 1] = box.current = Math.PI;
 		}
-	};
-	
-	this.memrecall = function() {
-		if (isNaN(this.display[this.display.length - 1])) {
-			this.display[this.display.length] = this.m;
-		}
-		else {
-			this.display[this.display.length - 1] = this.m;
-		}
-	};
-	
-	this.equals = function() {
-		console.log(this.display);
-		if (this.display.length === 2) {
-			this.display[2] = this.display[0];
-			this.compute();
-		}
-		if (this.display.length === 3) {
-			this.compute();
-		}
-	};
-	
-	this.num = function(id) {
-		this.temp += id;
-		if (isNaN(this.display[this.display.length - 1])) {
-			this.display[this.display.length] = parseFloat(this.temp);
-		}
-		else if (this.result !== 0) {
-			this.temp = id;
-			this.display = [parseFloat(this.temp)];
-		}
-		else {
-			this.display[this.display.length - 1] = parseFloat(this.temp);
-		}
-		this.result = 0;
-	};
-	
-	this.dot = function() {
-		if (this.display[this.display.length - 1] % 1 === 0) {
-			this.temp += ".";
-			this.display[this.display.length - 1] = parseFloat(this.temp);
-		}
-	};
-	
-	this.pi = function() {
-		if (isNaN(this.display[this.display.length - 1])) {
-			this.display[this.display.length] = Math.PI;
-		}
-		else {
-			this.display[this.display.length - 1] = Math.PI;
-		}
-	};
-	
-	this.back = function() {
-		if (this.result === 0) {
-			if (this.temp.length <= 1) {
-				this.temp = "0";
-			}
-			else {
-				this.temp = this.temp.substring(0, this.temp.length - 1);
-			}
-			this.display[this.display.length - 1] = parseFloat(this.temp);
-		}
-	};
-	
-	this.populateOperators = function(code) {
-		this.temp = "0";
-		if (this.display.length === 1) {
-			this.display[this.display.length] = code;
-		}
-		else if (this.display.length === 2) {
-			this.display[this.display.length - 1] = code;
-		}
-		else {
-			this.compute();
-			this.display[this.display.length] = code;
-		}
-	};
-	
-	this.clear = function() {
-		this.temp = "0";
-		this.display = [0];
-		this.result = 0;
-	};
+	},
 
-	this.sqroot = function() {
+	sqroot: function() {
 		this.display[this.display.length - 1] = this.result = Math.sqrt(this.display[this.display.length - 1]);
-	};
+	},
 
-	this.negate = function() {
-		this.display[this.display.length -1] *= (-1);
-		this.temp = this.display[this.display.length -1].toString();
-	};
+	negate: function() {
+		box.current *= (-1);
+	},
 
-	this.inverse = function() {
-		if (this.display[this.display.length -1] === 0) {
+	inverse: function() {
+		if (box.current === 0) {
 			alert("Cannot divide by 0.");
 		}
 		else {
 			this.display[this.display.length -1] = this.result = (1 / (this.display[this.display.length -1]));
 		}
-	};
-}
-
-
+	}
+};
 
 $(document).ready(function() {
+	input.getInput();
+});
 
-	var calc = new Calculator();
 
-	$('button').click(function() {
-
-		if (this.className === 'num') {
-			calc.num(this.innerHTML);
+var box = {
+	current: "",
+	inputs: ["0"],
+	result: "",
+	
+	populateCurrent: function(input) {
+		if (this.inputs[0] === "0" && this.inputs.length === 1) {
+			this.inputs = [input];
+			this.current = input;
+			console.log("a");
+			console.log(this.inputs);
 		}
-		else if (containsID(["add", "subtract", "multiply", "divide", "power"], this.id)) {
-			calc.populateOperators(this.innerHTML);
+		else if (this.inputs.length % 2 === 0) {
+			this.current = input;
+			this.inputs.push(input);
 		}
 		else {
-			calc[this.id]();
+			this.current += input;
+			this.inputs[this.inputs.length - 1] = this.current;
 		}
-		document.getElementById("display").innerHTML = calc.display.join(" ");
-	});
-});
+		console.log(this.inputs);
+	},
+	
+	showHistory: function() {
+		document.getElementById("history").innerHTML = this.inputs.join(" ");
+	},
+
+	showResult: function() {
+		document.getElementById("current").innerHTML = this.result;
+	},
+	
+	showCurrent: function() {
+		document.getElementById("current").innerHTML = this.current;
+	},
+	
+	populateOperators: function(operator) {
+		if (this.inputs.length % 2 === 0) {
+			this.inputs[this.inputs.length - 1] = operator;
+		}
+		else {
+			this.inputs.push(operator);
+		}
+		this.calcResult();
+		console.log(this.result);
+		this.current = "0";
+	},
+	
+	dot: function() {
+		if (this.current % 1 === 0) {
+			this.current += ".";
+		}
+	},
+	
+	clear: function() {
+		this.current = "";
+		this.inputs = ['0'];
+		this.result = "";
+	},
+	
+	back: function() {
+		if (this.inputs.length % 2 !== 0) {
+			this.current = this.current.substring(0, this.current.length - 1);
+			this.inputs[this.inputs.length - 1] = this.current;
+		}
+		this.showCurrent();
+	},
+	
+	calcResult: function() {
+		if (this.inputs.length <= 2) {
+			this.result = this.inputs[0];
+			console.log("a");
+		}
+		else if (this.inputs.length === 3) {
+			this.result = calc.compute(this.inputs);
+			console.log("b");
+		}
+		else if (this.inputs.length === 4) {
+			this.result = calc.compute(this.inputs.slice(0, this.inputs.length - 1));
+		}
+		else if (this.inputs.length % 2 === 0) {
+			console.log(this.result);
+			console.log([this.result, this.inputs[this.inputs.length - 3], this.inputs[this.inputs.length - 2]]);
+			this.result = calc.compute([this.result, this.inputs[this.inputs.length - 3], this.inputs[this.inputs.length - 2]]);
+			console.log("c");
+		}
+	},
+	
+	equals: function() {
+		this.calcResult();
+		this.inputs = [this.result];
+		this.current = "";
+	},
+};
+
+var input = {
+
+	getInput: function() {
+		$('button').click(function() {
+			if (this.className === 'num') {
+				box.populateCurrent(this.innerHTML);
+				box.showCurrent();
+			}
+			else if (containsID(["dot", "back"], this.id)) {
+				box[this.id]();
+			}
+			else if (containsID(["add", "subtract", "multiply", "divide", "power"], this.id)) {
+				box.populateOperators(this.innerHTML);
+				box.showResult();
+			}
+			else if (containsID(["clear", "equals"], this.id)) {
+				box[this.id]();
+				box.showResult();
+			}
+			else {
+				calc[this.id]();
+			}
+			box.showHistory();
+		});
+	}
+};
 
 function containsID(array, id) {
     for (var i = 0; i < array.length; i++) {
